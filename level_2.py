98b180dbf8debf9ccded4e88dfa356f453dba5f7
Madeline Blandini <madelineblandini@Madelines-MacBook-Pro-4.local>
2025-04-22 18:53:32 -0400

    Level 2 Complete

---
+++ level_2.py
@@ -0,0 +1,10 @@
+mix_of_stuff = [42, "Howdy, partner!", 3.14, False]
+print(mix_of_stuff)
+
+ 
+sequence = ["Pure imagination - VXLLAIN, Kenny Gray", "Bloody Mary (Slowed & Reverb) - Polsas", "Embrace - Pastel Ghost", "Skins - KREZUS", "dandelion - Ariana Grande"]
+print(sequence[4])
+
+playlist_for_coding = ["Pure imagination - VXLLAIN, Kenny Gray", "Bloody Mary (Slowed & Reverb) - Polsas", "Embrace - Pastel Ghost", "Skins - KREZUS", "dandelion - Ariana Grande"]
+playlist_for_coding.append("Never Gonna Give You Up - Rick Astley")
+print(playlist_for_coding)
